b3782ee344eb9ad3d08f73ec883f1f5d9e458b9c
ridwan-101 <abdulazeezridwan004@gmail.com>
2023-08-15 15:23:13 +0100

    collection of data to entry record 👾

---
+++ lib/model/registrationdata.dart
@@ -0,0 +1,13 @@
+class RegistrationData {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String purpose;
+
+  RegistrationData({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.purpose,
+  });
+}
